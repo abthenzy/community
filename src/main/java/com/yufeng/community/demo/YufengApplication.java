1a3156b2c758e3bbcadfc1634108ec13afa0afb1
abthen <18864240@qq.com>
2019-10-23 01:22:43 +0800

    init repo

---
+++ src/main/java/com/yufeng/community/demo/YufengApplication.java
@@ -0,0 +1,13 @@
+package com.yufeng.community.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YufengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YufengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yufeng/community/demo/YufengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yufeng.community.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YufengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
